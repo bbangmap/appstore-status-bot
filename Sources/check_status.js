b7c0f6f596536c862b1a8ddd98c60ef1334c7e3c
junhyeon.oh <oh.junhyeon95@gmail.com>
2022-12-21 14:19:12 +0900

    조건 명시화

--- Sources/check_status.js
+++ Sources/check_status.js
@@ -32,11 +32,16 @@ const checkVersion = async (app, gist) => {
   console.log("[*] checkVersion");
   var app = app[0];
   app["submission_start_date"] = gist.submission_start_date;
+
   var currentDay = app.app_store_version_phased_release.current_day_number
   var phased_release_state = app.app_store_version_phased_release.phased_release_state
   app["phase_percentage"] = calculatePercentage(currentDay, phased_release_state)
 
-  if (!app.appID || phased_release_state != gist.app_store_version_phased_release.phased_release_state || app.status != gist.status || phased_release_state.current_day_number != gist.app_store_version_phased_release.current_day_number) {
+  var isEqualPhasesState = app.app_store_version_phased_release.phased_release_state == gist.app_store_version_phased_release.phased_release_state
+  var isEqualPhasesDay = app.app_store_version_phased_release.current_day_number == gist.app_store_version_phased_release.current_day_number
+  console.log(`[*] isEqualPhasesState: ${isEqualPhasesState} isEqualPhasesDay: ${isEqualPhasesDay}`);
+
+  if (!app.appID || !isEqualPhasesState || app.status != gist.status || !isEqualPhasesDay) {
     console.log("[*] status is different");
 
     var submission_start_date = gist.submission_start_date
